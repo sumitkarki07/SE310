0c245d31e4c7571421f651be0dc68579d1a1d39d
sumit karki <sumitkarki@sumits-MacBook-Pro-3.local>
2026-04-07 09:53:38 -0500

    Initial setup

---
+++ SE310.py
@@ -0,0 +1,17 @@
+team_name="GDG Hackers"
+
+def addition(number1, number2):
+    print("We are adding " + str(number1) + " and " + str(number2))
+    return number1 + number2
+    def calculator():
+        print("Calculator by team = " + team_name)
+        print("Choose the operation you want to perform: ")
+        print("1. Addition")
+        print("2. Subtraction")
+        print("3. Multiplication")
+        print("4. Division")
+        print("5. Integer Division")
+        print("6. Square Root")
+        print("7. Exponent")
+        choice = int(input("Enter your choice: "))
+calculator()
